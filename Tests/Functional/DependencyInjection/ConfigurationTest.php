f8ac7b122e188dc1b4cb6dcff752d7f5d26408ea
Michael Babker <michael.babker@gmail.com>
2022-04-20 19:47:09 -0500

    Require the refresh_token_class config node to be set

--- Tests/Functional/DependencyInjection/ConfigurationTest.php
+++ Tests/Functional/DependencyInjection/ConfigurationTest.php
@@ -19,7 +19,11 @@ final class ConfigurationTest extends TestCase
 
     public function test_default_configuration_is_valid(): void
     {
-        $this->assertConfigurationIsValid([]);
+        $this->assertConfigurationIsValid([
+            [
+                'refresh_token_class' => RefreshToken::class,
+            ]
+        ]);
     }
 
     public function test_custom_configuration_is_valid(): void
@@ -42,6 +46,15 @@ final class ConfigurationTest extends TestCase
                     'http_only' => false,
                 ],
             ],
+        ]);
+    }
+
+    public function test_configuration_is_invalid_when_refresh_token_class_does_not_implement_the_required_interface(): void
+    {
+        $this->assertConfigurationIsInvalid([
+            [
+                'refresh_token_class' => Configuration::class,
+            ]
         ]);
     }
 }
